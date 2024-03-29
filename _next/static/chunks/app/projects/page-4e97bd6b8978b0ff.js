565901177c53bc8f3a01fc6872e59ac091ad0d0b
Joe1Ontiveros <jontiverosrodriguez1@gmail.com>
2024-03-29 15:45:34 -0600

    Static Site Exported
    Debugging - Cleanup soon

---
+++ _next/static/chunks/app/projects/page-4e97bd6b8978b0ff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[895],{4296:function(n,e,u){Promise.resolve().then(u.bind(u,4191))}},function(n){n.O(0,[41,371,971,69,744],function(){return n(n.s=4296)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/research/page-7206b6d2845251e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[214],{8751:function(n,e,u){Promise.resolve().then(u.bind(u,4105))}},function(n){n.O(0,[41,371,971,69,744],function(){return n(n.s=8751)}),_N_E=n.O()}]);
