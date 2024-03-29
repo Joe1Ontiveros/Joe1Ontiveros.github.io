afbbb6ccf878385394d4809f06681e917ecac023
Joe <jontiverosrodriguez1@gmail.com>
2024-03-29 17:28:50 -0600

    dependency fix 1
    react-router-dom was not declared in dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const nextConfig = {
     // basePath: "/portfolio-joe-react1",
     output: "export",  // <=== enables static exports
-    
     reactStrictMode: true,
+
   };
   module.exports = {
     useFileSystemPublicRoutes: false,
+    
   }
   module.exports = nextConfig
