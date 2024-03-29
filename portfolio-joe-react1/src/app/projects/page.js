a709ca66e775296fc8e3cde9b141908b97437c1e
Joe <jontiverosrodriguez1@gmail.com>
2024-03-28 23:15:38 -0600

    Non-Static NextJS Base-Site
    For backup purposes, later converted to a static site for github pages

---
+++ portfolio-joe-react1/src/app/projects/page.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Header } from "dev/comp/Header";
+import {Home} from "dev/app/page";
+import "./style.css";
+import Image from 'next/image'
+
+
+const Projects = ({ className }) => {
+  // const images = [
+  //   {
+  //     src: './img1.jpeg',
+  //     alt: 'SRI Slide demo view',
+  //   },
+  //   // ... more images
+  // ]
+  return (
+    //   <div className={`EXPERIENCE`}>
+    <div>
+            <p>
+                Work in progress! Coming Soon!!!
+            </p>
+    </div>
+  );
+};
+
+export default Projects;
